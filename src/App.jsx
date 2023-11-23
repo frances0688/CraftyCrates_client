65af6bd7895317874c72755aabab387098967593
Frances Morales Velilla <frances0688@gmail.com>
2023-11-23 21:07:26 +0100

    Create component files, add color scheme.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css'
+import './App.scss'
+import Box from './components/Box/Box'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Item from './components/Item/Item'
 
 function App() {
 
   return (
 
-    <div>App</div>
+    <div>
+      <Header/>
+      <Box/>
+      <Item/>
+      <Footer/>
+    </div>
 
   )
 }
